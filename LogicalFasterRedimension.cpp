1ee0a4feed5763bc5ba5035d8463cd6af73c3cc9
Alex Poliakov <apoliakov@paradigm4.com>
2016-08-04 19:08:57 -0400

    Schema inference

--- LogicalFasterRedimension.cpp
+++ LogicalFasterRedimension.cpp
@@ -37,22 +37,116 @@ public:
         LogicalOperator(logicalName, alias)
     {
         ADD_PARAM_INPUT();
+        ADD_PARAM_SCHEMA();
     }
-
-//    std::vector<shared_ptr<OperatorParamPlaceholder> > nextVaryParamPlaceholder(const std::vector< ArrayDesc> &schemas)
-//    {
-//        std::vector<shared_ptr<OperatorParamPlaceholder> > res;
-//        res.push_back(END_OF_VARIES_PARAMS());
-//        if (_parameters.size() < Settings::MAX_PARAMETERS)
-//        {
-//            res.push_back(PARAM_CONSTANT("string"));
-//        }
-//        return res;
-//    }
 
     ArrayDesc inferSchema(vector< ArrayDesc> schemas, shared_ptr< Query> query)
     {
-        return schemas[0];
+        assert(schemas.size() == 1);
+
+        ArrayDesc const& srcDesc = schemas[0];
+        ArrayDesc dstDesc = ((std::shared_ptr<OperatorParamSchema>&)_parameters[0])->getSchema();
+
+        if (!dstDesc.getEmptyBitmapAttribute())
+        {
+            throw USER_EXCEPTION(SCIDB_SE_INFER_SCHEMA, SCIDB_LE_OP_REDIMENSION_ERROR1);
+        }
+
+        //Ensure attributes names uniqueness.
+        size_t numPreservedAttributes = 0;
+        for (const AttributeDesc &dstAttr : dstDesc.getAttributes())
+        {
+            // Look for dstAttr among the source and aggregate attributes.
+            for (const AttributeDesc &srcAttr : srcDesc.getAttributes())
+            {
+                if (srcAttr.getName() == dstAttr.getName())
+                {
+                    if (srcAttr.getType() != dstAttr.getType())
+                    {
+                        throw USER_EXCEPTION(SCIDB_SE_INFER_SCHEMA, SCIDB_LE_WRONG_ATTRIBUTE_TYPE)
+                        << srcAttr.getName() << srcAttr.getType() << dstAttr.getType();
+                    }
+                    if (!dstAttr.isNullable() && srcAttr.isNullable())
+                    {
+                        throw USER_EXCEPTION(SCIDB_SE_INFER_SCHEMA, SCIDB_LE_WRONG_ATTRIBUTE_FLAGS)
+                        << srcAttr.getName();
+                    }
+                    if (!srcAttr.isEmptyIndicator())
+                    {
+                        ++numPreservedAttributes;
+                    }
+                    goto NextAttr;
+                }
+            }
+
+            // Not among the source attributes, look for it among source dimensions (copied to
+            // aggregationDesc above).
+            for (const DimensionDesc &srcDim : srcDesc.getDimensions())
+            {
+                if (srcDim.hasNameAndAlias(dstAttr.getName()))
+                {
+                    if (dstAttr.getType() != TID_INT64)
+                    {
+                        throw USER_EXCEPTION(SCIDB_SE_INFER_SCHEMA, SCIDB_LE_WRONG_DESTINATION_ATTRIBUTE_TYPE)
+                        << dstAttr.getName() << TID_INT64;
+                    }
+                    goto NextAttr;
+                }
+            }
+
+            // This dstAttr should now be accounted for (i.e. we know where it's derived from), so
+            // if we get here then this one better be the emptyBitmap.
+            if (dstAttr.isEmptyIndicator() == false)
+            {
+                throw USER_EXCEPTION(SCIDB_SE_INFER_SCHEMA, SCIDB_LE_UNEXPECTED_DESTINATION_ATTRIBUTE)
+                << dstAttr.getName();
+            }
+        NextAttr:;
+        }
+
+        // Similarly, make sure we know how each dstDim is derived.
+        Dimensions outputDims;
+        size_t nNewDims = 0;
+        for (const DimensionDesc &dstDim : dstDesc.getDimensions())
+        {
+            int64_t interval = dstDim.getChunkIntervalIfAutoUse(std::max(1L, dstDim.getChunkOverlap()));
+            if (dstDim.getChunkOverlap() > interval)
+            {
+                throw USER_EXCEPTION(SCIDB_SE_INFER_SCHEMA, SCIDB_LE_OVERLAP_CANT_BE_LARGER_CHUNK);
+            }
+            for (const AttributeDesc &srcAttr : srcDesc.getAttributes())
+            {
+                if (dstDim.hasNameAndAlias(srcAttr.getName()))
+                {
+                    if ( !IS_INTEGRAL(srcAttr.getType())  || srcAttr.getType() == TID_UINT64 )
+                    {
+                        // (TID_UINT64 is the only integral type that won't safely convert to TID_INT64.)
+                        throw USER_EXCEPTION(SCIDB_SE_INFER_SCHEMA, SCIDB_LE_WRONG_SOURCE_ATTRIBUTE_TYPE)
+                            << srcAttr.getName();
+                    }
+                    outputDims.push_back(dstDim);
+                    goto NextDim;
+                }
+            }
+            for (const DimensionDesc &srcDim : srcDesc.getDimensions())
+            {
+                if (srcDim.hasNameAndAlias(dstDim.getBaseName()))
+                {
+                    DimensionDesc outputDim = dstDim;
+                    outputDims.push_back(outputDim);
+                    goto NextDim;
+                }
+            }
+            throw SYSTEM_EXCEPTION(SCIDB_SE_INTERNAL, SCIDB_LE_ILLEGAL_OPERATION) << "no extraneous dimensions allowed";
+        NextDim:;
+        }
+
+        return ArrayDesc(srcDesc.getName(),
+                         dstDesc.getAttributes(),
+                         outputDims,
+                         createDistribution(psUndefined),
+                         query->getDefaultArrayResidency(),
+                         dstDesc.getFlags());
     }
 };
 
